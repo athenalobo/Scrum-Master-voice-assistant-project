b9bd696d0cdd8a9cb2e77257529c9f890cd1f5a8
athenalobo <athenamlobo@gmail.com>
2025-02-12 18:20:29 +0530

    settings.py

--- models/settings.py
+++ models/settings.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
-
-class Settings(BaseModel):
-    jira_username: str
-    jira_token: str
-    jira_server: str
-    project_keys: list = []
+from typing import List
+class Settings():
+    def __init__(self, jira_username: str, jira_token: str, jira_server: str, project_keys: List[str]):
+        self.jira_username= jira_username
+        self.jira_token= jira_token
+        self.jira_server= jira_server
+        self.project_keys= project_keys
